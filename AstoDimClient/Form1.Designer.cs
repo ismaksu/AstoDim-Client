6246c9933d8006c4b7c2490989380d3b23c8f4a5
ismaksu <mcpebagimli@gmail.com>
2025-07-29 16:24:28 +0300

    Button background image layout changed from tile to stretch

--- AstoDimClient/Form1.Designer.cs
+++ AstoDimClient/Form1.Designer.cs
@@ -45,23 +45,24 @@
             // 
             mskLicenseKey.BackColor = Color.Black;
             mskLicenseKey.BorderStyle = BorderStyle.None;
-            mskLicenseKey.Font = new Font("VCR OSD Mono", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            mskLicenseKey.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             mskLicenseKey.ForeColor = Color.FromArgb(162, 0, 0);
-            mskLicenseKey.Location = new Point(486, 213);
+            mskLicenseKey.Location = new Point(555, 284);
+            mskLicenseKey.Margin = new Padding(3, 4, 3, 4);
             mskLicenseKey.Mask = ">AAAAA-AAAAA-AAAAA-AAAAA-AAAAA";
             mskLicenseKey.Name = "mskLicenseKey";
-            mskLicenseKey.Size = new Size(413, 24);
+            mskLicenseKey.Size = new Size(472, 34);
             mskLicenseKey.TabIndex = 2;
             mskLicenseKey.Enter += mskLicenseKey_Enter;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("VCR OSD Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(162, 0, 0);
-            label1.Location = new Point(484, 183);
+            label1.Location = new Point(553, 244);
             label1.Name = "label1";
-            label1.Size = new Size(194, 19);
+            label1.Size = new Size(187, 29);
             label1.TabIndex = 3;
             label1.Text = "Lisans Anahtari: ";
             // 
@@ -69,11 +70,11 @@
             // 
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
-            label3.Font = new Font("VCR OSD Mono", 9.75F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            label3.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Underline, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.DarkRed;
-            label3.Location = new Point(12, 566);
+            label3.Location = new Point(14, 755);
             label3.Name = "label3";
-            label3.Size = new Size(125, 14);
+            label3.Size = new Size(126, 20);
             label3.TabIndex = 6;
             label3.Text = "Lisans Anahtari";
             // 
@@ -81,11 +82,11 @@
             // 
             lblLicenseKey.AutoSize = true;
             lblLicenseKey.BackColor = Color.Transparent;
-            lblLicenseKey.Font = new Font("VCR OSD Mono", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblLicenseKey.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblLicenseKey.ForeColor = Color.DarkRed;
-            lblLicenseKey.Location = new Point(12, 584);
+            lblLicenseKey.Location = new Point(14, 779);
             lblLicenseKey.Name = "lblLicenseKey";
-            lblLicenseKey.Size = new Size(189, 14);
+            lblLicenseKey.Size = new Size(308, 20);
             lblLicenseKey.TabIndex = 7;
             lblLicenseKey.Text = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX";
             // 
@@ -97,9 +98,10 @@
             btnHideKey.FlatAppearance.BorderSize = 0;
             btnHideKey.FlatStyle = FlatStyle.Flat;
             btnHideKey.ForeColor = Color.DarkRed;
-            btnHideKey.Location = new Point(255, 572);
+            btnHideKey.Location = new Point(291, 763);
+            btnHideKey.Margin = new Padding(3, 4, 3, 4);
             btnHideKey.Name = "btnHideKey";
-            btnHideKey.Size = new Size(30, 30);
+            btnHideKey.Size = new Size(34, 40);
             btnHideKey.TabIndex = 8;
             btnHideKey.UseVisualStyleBackColor = false;
             btnHideKey.Visible = false;
@@ -107,11 +109,11 @@
             // 
             // lblRemaining
             // 
-            lblRemaining.Font = new Font("VCR OSD Mono", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRemaining.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblRemaining.ForeColor = Color.FromArgb(162, 0, 0);
-            lblRemaining.Location = new Point(452, 405);
+            lblRemaining.Location = new Point(517, 540);
             lblRemaining.Name = "lblRemaining";
-            lblRemaining.Size = new Size(471, 19);
+            lblRemaining.Size = new Size(538, 25);
             lblRemaining.TabIndex = 14;
             lblRemaining.Text = "Lisansin kalan süresi: 30 gün";
             lblRemaining.TextAlign = ContentAlignment.MiddleCenter;
@@ -120,11 +122,11 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("VCR OSD Mono", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.FromArgb(162, 0, 0);
-            label2.Location = new Point(486, 76);
+            label2.Location = new Point(555, 101);
             label2.Name = "label2";
-            label2.Size = new Size(289, 42);
+            label2.Size = new Size(291, 60);
             label2.TabIndex = 16;
             label2.Text = "System Initialization Status: True\r\nCooler Systems: Active\r\nBypassing Security Firewall: Success";
             // 
@@ -141,11 +143,12 @@
             btnActivateLicense.FlatAppearance.MouseDownBackColor = Color.Transparent;
             btnActivateLicense.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btnActivateLicense.FlatStyle = FlatStyle.Flat;
-            btnActivateLicense.Font = new Font("VCR OSD Mono", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnActivateLicense.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnActivateLicense.ForeColor = Color.FromArgb(192, 0, 0);
-            btnActivateLicense.Location = new Point(728, 243);
+            btnActivateLicense.Location = new Point(832, 324);
+            btnActivateLicense.Margin = new Padding(3, 4, 3, 4);
             btnActivateLicense.Name = "btnActivateLicense";
-            btnActivateLicense.Size = new Size(171, 50);
+            btnActivateLicense.Size = new Size(195, 67);
             btnActivateLicense.TabIndex = 17;
             btnActivateLicense.Text = "Aktiflestir";
             btnActivateLicense.UseVisualStyleBackColor = true;
@@ -156,15 +159,17 @@
             // btnInjectBot
             // 
             btnInjectBot.BackgroundImage = Properties.Resources.popos1;
+            btnInjectBot.BackgroundImageLayout = ImageLayout.Stretch;
             btnInjectBot.FlatAppearance.BorderSize = 0;
             btnInjectBot.FlatAppearance.MouseDownBackColor = Color.Transparent;
             btnInjectBot.FlatAppearance.MouseOverBackColor = Color.Transparent;
             btnInjectBot.FlatStyle = FlatStyle.Flat;
-            btnInjectBot.Font = new Font("VCR OSD Mono", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnInjectBot.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnInjectBot.ForeColor = Color.Red;
-            btnInjectBot.Location = new Point(517, 277);
+            btnInjectBot.Location = new Point(591, 369);
+            btnInjectBot.Margin = new Padding(3, 4, 3, 4);
             btnInjectBot.Name = "btnInjectBot";
-            btnInjectBot.Size = new Size(335, 125);
+            btnInjectBot.Size = new Size(383, 167);
             btnInjectBot.TabIndex = 18;
             btnInjectBot.Text = "Botu Enjekte Et";
             btnInjectBot.UseVisualStyleBackColor = true;
@@ -175,12 +180,12 @@
             // 
             // frmClientMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
             BackgroundImage = Properties.Resources.ultrakill_red_black_machine_robot_hd_wallpaper_08962df850a0cc58b08cc10e58b2040a3;
             BackgroundImageLayout = ImageLayout.Zoom;
-            ClientSize = new Size(926, 607);
+            ClientSize = new Size(1058, 809);
             Controls.Add(btnInjectBot);
             Controls.Add(btnActivateLicense);
             Controls.Add(label2);
@@ -192,6 +197,7 @@
             Controls.Add(mskLicenseKey);
             DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
             Name = "frmClientMain";
             StartPosition = FormStartPosition.CenterScreen;
